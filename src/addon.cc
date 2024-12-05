9618f705e3269b74bc8487c93626f1be0bd7d971
ShaMan123 <shacharnen@gmail.com>
2024-12-05 23:49:33 +0200

    upgrade to @electron/remote + rm `electron-settings`
    bump
    
    comply with nan context aware module
    
    cleanup some code

--- src/addon.cc
+++ src/addon.cc
@@ -1,22 +1,19 @@
 #include <nan.h>
-//#include "minkowski.h"
+// #include "minkowski.h"
 
+using Nan::GetFunction;
+using Nan::New;
+using Nan::Set;
 using v8::FunctionTemplate;
 using v8::Local;
 using v8::Object;
 using v8::String;
-using Nan::GetFunction;
-using Nan::New;
-using Nan::Set;
 
 NAN_METHOD(calculateNFP);
 
-// Expose synchronous and asynchronous access to our
-// Estimate() function
-NAN_MODULE_INIT(InitAll) {
-  Set(target, New<String>("calculateNFP").ToLocalChecked(),
-    GetFunction(New<FunctionTemplate>(calculateNFP)).ToLocalChecked());
+NAN_MODULE_INIT(Init)
+{
+  Nan::SetMethod(target, "calculateNFP", calculateNFP);
 }
 
-NODE_MODULE(addon, InitAll);
-
+NAN_MODULE_WORKER_ENABLED(addon, Init)
